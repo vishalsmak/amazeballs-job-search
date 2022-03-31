574a9ab166f57ae1f9d34759adddabcadda7c20c
Namrata K <namrata.kankaria@gmail.com>
2022-03-31 21:35:37 +0100

    flask setup

--- src/app.py
+++ src/app.py
@@ -1,13 +1,28 @@
 from flask import Flask
+from flask_cors import CORS
 
 from src.config import config as cfg
+from src.views.auth import auth_app
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 app.config["SECRET_KEY"] = cfg.get("common", "SECRET_KEY")
 
+if cfg.get("flask", "DEBUG", fallback=False):
+    print("CORS enabled for the app !!")
+    CORS(app)
 
-debug = cfg.get("common", "DEBUG", fallback=False)
-host = cfg.get("common", "HOST")
-port = cfg.get("common", "PORT")
-app.run(debug=debug, host=host, port=port)
+# register blueprints
+app.register_blueprint(auth_app)
+
+
+@app.route("/health_check", methods=["GET"])
+def verify():
+    return "Hurray!! Happy that I am alive. Have a good day :)"
+
+
+app.run(
+    host="0.0.0.0",
+    debug=cfg.get("common", "DEBUG", fallback=False),
+    port=cfg.get("common", "PORT", fallback=5000),
+)
--- src/views/auth.py
+++ src/views/auth.py
@@ -1,18 +1,18 @@
-from flask import flash, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, url_for
 
-from src.app import app
 from src.forms.login import LoginForm
 from src.forms.registration import RegistrationForm
 from src.local_data import jobs
 
+auth_app = Blueprint("auth", __name__)
 
-@app.route("/")
-@app.route("/home")
+
+@auth_app.route("/auth/home", methods=["GET"])
 def home():
     return render_template("home.html", jobs=jobs)
 
 
-@app.route("/register", methods=["GET", "POST"])
+@auth_app.route("/auth/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -21,7 +21,7 @@ def register():
     return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login", methods=["GET", "POST"])
+@auth_app.route("/auth/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -36,6 +36,6 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
-@app.route("/about")
+@auth_app.route("/auth/about", methods=["GET"])
 def about():
     return render_template("about.html", title="About")
