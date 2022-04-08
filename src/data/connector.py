0fa69c0434ad0c2056d25a9f92e6503d01db1e9d
Loveshark <loveshark@192.168.1.105>
2022-04-08 20:03:27 +0100

    adding connector

--- src/data/connector.py
+++ src/data/connector.py
@@ -1,6 +1,4 @@
-import urllib.parse
-
-import pymongo
+from pymongo import MongoClient
 
 from app.config import config as cfg
 
@@ -11,7 +9,8 @@ class Connection:
     USER_NAME = None
     PASSWORD = None
     DB_NAME = None
-    _CLIENT = None
+    CONNECTION = False
+    _DB = None
 
     def __init__(self):
         self.HOST = cfg.get("mongo", "HOST")
@@ -20,25 +19,27 @@ class Connection:
         self.PASSWORD = cfg.get("mongo", "PASSWORD")
         self.DB_NAME = cfg.get("mongo", "DB_NAME")
 
-    def get_client(self):
-        if not self._CLIENT:
-            self._CLIENT = pymongo.MongoClient(
-                f"mongodb://{self.USER_NAME}:{urllib.parse.quote_plus(self.PASSWORD)}@{self.HOST}:{self.PORT}/{self.DB_NAME}"
+    def get_db(self):
+        if not self.CONNECTION:
+            client = MongoClient(
+                f"mongodb://{self.USER_NAME}:{self.PASSWORD}@{self.HOST}:{self.PORT}"
             )
-        return self._CLIENT
+            self._DB = client[self.DB_NAME]
+            self.CONNECTION = True
+        return self._DB
 
     def get_collections(self):
-        qmul = self.get_client().qmul
+        qmul = self.get_db()
         qmul.list_collection_names()
 
     def get_jobs(self):
-        qmul = self.get_client().qmul
-        data = qmul.jobs
+        qmul = self.get_db()
+        data = qmul["jobs"]
         return data
 
     def push_jobs(self, jobs):
-        qmul = self.get_client().qmul
-        collection = qmul.jobs
+        qmul = self.get_db()
+        collection = qmul["jobs"]
         collection.insert_many(jobs)
         return
 
