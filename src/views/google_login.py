00860570b3025ee3c3a289d21ad23617ab6284ec
Loveshark <loveshark@192.168.1.105>
2022-04-09 01:01:20 +0100

    adding users

--- src/views/google_login.py
+++ src/views/google_login.py
@@ -4,6 +4,7 @@ import requests
 from flask import Blueprint, Response, render_template, request
 
 from app.config import config as cfg
+from src.data.connector import mongo_db
 
 google_app = Blueprint("google", __name__)
 scope = "profile email openid"
@@ -24,7 +25,7 @@ def get_google_url():
         querystring = urlencode(data)
         url = f"https://accounts.google.com/o/oauth2/v2/auth?{querystring}"
     except Exception as error:
-        print("get_google_url create {}".format(error))
+        print("get_google_url {}".format(error))
     return url
 
 
@@ -32,22 +33,34 @@ def get_google_url():
 def callback():
     print("Called by google!!")
     try:
+        callback_url = f"{cfg.get('google', 'REDIRECT_URI')}/google/callback"
         response = requests.post(
             url="https://www.googleapis.com/oauth2/v4/token",
             data={
                 "code": request.args.get("code"),
-                "redirect_uri": cfg.get("google", "REDIRECT_URI"),
+                "redirect_uri": callback_url,
                 "client_id": cfg.get("google", "CLIENT_ID"),
                 "client_secret": cfg.get("google", "CLIENT_SECRET"),
                 "grant_type": "authorization_code",
             },
-        ).content
+        ).json()
+
+        response = requests.get(
+            "https://www.googleapis.com/oauth2/v1/userinfo?alt=json",
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(response["access_token"]),
+            },
+        ).json()
 
         email = response["email"]
         first_name = response["given_name"]
         last_name = response["family_name"]
         attributes = {"google_data": response}
-        print(email, first_name, last_name, attributes)
+
+        if not mongo_db.get_user(email):
+            mongo_db.save_user(email, first_name, last_name, attributes)
+
         return render_template(
             "google_response.html",
             title="Google Response",
@@ -56,5 +69,5 @@ def callback():
             last_name=last_name,
         )
     except Exception as error:
-        print("callback create {}".format(error))
+        print("callback {}".format(error))
         return Response(status=500)
